21e10851ffd4d91bb1e69d1baaf7728c1b7f187b
z-ferrum <z-ferrum@mail.ru>
2016-11-03 18:31:07 +0400

    Создали класс Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point //новый класс. Класс хранит данные точки
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw() //функцию можно убирать в класс, вызывается через .draw
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,10 +8,45 @@ namespace Snake
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Все что внутри этого блока - основной код программы
         {
-            Console.WriteLine("Hello world");
+            /* Это деревенский способ вывести точку на консоль
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+            */
+
+            /* Колхозный способ задать точку
+             int x1 = 1;
+             int y1 = 3;
+             char sym1 = '*';
+
+             Draw(x1, y1, '*'); //но уже вывод через функцию
+             */
+
+            Point p1 = new Point(); //new пишется так как это класс
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point(); //new пишется так как это класс
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+                    
             Console.ReadLine();
         }
+
+        /* 
+        static void Draw(int x, int y, char sym) //функция вывода точки на консоль
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }*/
     }
 }
